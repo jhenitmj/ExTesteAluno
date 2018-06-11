eee3f71e72a7a6d3466785a6ccfc0b1cae5c3e1b
jhenitmj <jheni.tmj@outlook.com>
2018-06-11 20:37:15 -0300

    CommitTeste

---
+++ Exercicioteste/Exercicioteste/Aluno.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Exercicioteste
+{
+   public class Aluno
+    {
+        public int cod;
+       public DateTime data;
+       public string nomep;
+        public string sobrenome;
+        public string NomeCompleto;
+        public int obj;
+
+
+
+        public int idade()
+        { // este método deverá calcular a idade do aluno baseado na sua data de nascimento
+
+
+            DateTime dataAtual = DateTime.Now;
+
+            //Subtai para saber quantos anos se passaram após nascimento
+            int idade = DateTime.Now.Year - data.Year;
+
+            return idade;
+        }
+        public string nomeCompleto()
+        {// Deverá retornar o nome concatenado com o sobrenome
+            
+            NomeCompleto = nomep.Trim() + "" + sobrenome;
+            return NomeCompleto;
+
+        }
+
+        public int validaNome(bool CaractereE)
+        {//Deverá validar o nome do Aluno com caracteres divergentes
+            if (CaractereE = Regex.IsMatch(NomeCompleto, (@"[!""#$%&'()*+,-./:;?@[\\\]_`{|}~]")))
+            {
+                obj = 1;
+            }
+            else if (nomep.StartsWith("Ç") || sobrenome.StartsWith("Ç"))
+            {
+                obj = 2;
+            }
+
+            return obj;
+                                                    
+        }
+
+        public string erroValidacao()
+        {
+            if(obj==1)
+            {
+                Console.WriteLine("Este nome não pode ser usado contém caracter 'Ç' que é inválido no inicio do nome ");
+            }
+            else if(obj ==2)
+                {
+                Console.WriteLine("Este nome não pode ser usado contém caracter 'Ç' que é inválido no inicio do nome ");
+            }
+            return erroValidacao();
+        }
+    }
+}
---
+++ Exercicioteste/Test1exe/UnitTest1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exercicioteste;
+
+namespace Test1exe
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Aluno aluno = new Aluno();
+            aluno.cod = 1;
+            aluno.nomep = "Felipe Vozgeral Çanca";
+            aluno.data = new DateTime(1990, 04, 01);
+            Assert.IsFalse(false);
+
+
+        }
+
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Aluno aluno = new Aluno();
+            aluno.cod = 2;
+            aluno.nomep = "Alin@  Branca";
+            aluno.data = new DateTime(2020, 01, 01);
+            Assert.IsFalse(false);
+
+
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Aluno aluno = new Aluno();
+            aluno.cod = 3;
+            aluno.nomep = " Alfredo Sella III";
+            aluno.data = new DateTime(1986, 12, 03);
+            Assert.IsFalse(false);
+
+
+        }
+
+
+
+
+    }
+}
